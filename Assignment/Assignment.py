2485914c2978c3c418facffc862d4c9ec5a33fd3
Thomas-Harris <tlharr@essex.ac.uk>
2018-02-20 14:52:58 +0000

    Assignment

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -1,16 +1,17 @@
 #! /usr/bin/python3
 import os, random
+from PIL import Image
 
 language = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background"))
-print(language)
+#print(language)
 
 filename1 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language))
-print(filename1)
+#print(filename1)
 image1 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language+"/"+filename1))
 print(image1)
 
 filename2 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language))
-print(filename2)
+#print(filename2)
 image2 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language+"/"+filename2))
 print(image2)
 
@@ -19,4 +20,15 @@ if filename1 == filename2:
 else:
 	dif = 1
 print(dif)
+
+im1 = Image.open("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language+"/"+filename1+"/"+image1)
+im2 = Image.open("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language+"/"+filename2+"/"+image2)
+
+#im1.show()
+image1array = list(im1.getdata())
+image2array = list(im2.getdata())
+
+#create two numpy arrays one with images in (x) and one with same or different (y)
+# [image 1, image 2,			[ 1,
+#  image 1, image 2]			  0 ]
 
