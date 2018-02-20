0091b702fa2395e576111158cdecfc9837af632e
Thomas-Harris <tlharr@essex.ac.uk>
2018-02-20 13:50:23 +0000

    Assignment

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -11,7 +11,7 @@ print(image1)
 
 filename2 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language))
 print(filename2)
-image2 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language+"/"+filename1))
+image2 = random.choice(os.listdir("/home/mlvm2/ce888lab/Assignment/omniglot-master/python/images_background/"+language+"/"+filename2))
 print(image2)
 
 if filename1 == filename2:
